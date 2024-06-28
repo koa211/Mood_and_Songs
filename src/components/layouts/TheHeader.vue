eb768cb8e8b251dd7f6825ad015336c3878c411d
KhoaLG <legiakhoa0@gmail.com>
2024-06-28 16:30:38 +0700

    Basic layout of website idea: Header with title Left half will be a list of questions Right half will be the output of the song

---
+++ src/components/layouts/TheHeader.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+  <h1>Can you hear the music</h1>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/pages/App.vue
@@ -0,0 +1,54 @@
+<script setup>
+import HelloWorld from '../components/layouts/HelloWorld.vue'
+import TheWelcome from '../components/layouts/TheWelcome.vue'
+import TheHeader from "@/components/layouts/TheHeader.vue";
+</script>
+
+<template>
+  <header>
+    <TheHeader></TheHeader>
+  </header>
+
+  <main>
+    <img alt="Vue logo" class="logo" src="../assets/logo.svg" width="50" height="50" />
+
+    <div class="wrapper">
+      <HelloWorld msg="Can you hear the music" />
+    </div>
+  </main>
+</template>
+
+<style scoped>
+header {
+  position: fixed;   /* Fix the header position */
+  top: 0;            /* Align to the top of the viewport */
+  left: 0;           /* Align to the left of the viewport */
+  width: 100%;       /* Take full width of the viewport */
+  background-color: #333; /* Example background color */
+  color: white;      /* Example text color */
+  padding: 10px;
+}
+
+header h1 {
+  margin-left: 20px;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
