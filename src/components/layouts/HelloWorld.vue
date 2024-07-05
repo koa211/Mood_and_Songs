a73d28c3969456f1f1deffb2c2b93e4ed0f63ac7
KhoaLG <legiakhoa0@gmail.com>
2024-07-05 16:59:26 +0700

    Basic layout of website idea: Header with title Left half will be a list of questions Right half will be the output of the song

--- src/components/layouts/HelloWorld.vue
+++ src/components/layouts/HelloWorld.vue
@@ -1,10 +1,21 @@
 <script setup>
+import { ref } from 'vue';
+
 defineProps({
   msg: {
     type: String,
     required: true
   }
 })
+
+const mood = ref('');
+const moodOptions = [
+  { text: 'Happy', value: 'happy' },
+  { text: 'Sad', value: 'sad' },
+  { text: 'Irritated', value: 'irritated' },
+  { text: 'Anxious', value: 'anxious' },
+  { text: 'Calm', value: 'calm' }
+];
 </script>
 
 <template>
@@ -16,13 +27,45 @@ defineProps({
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
     </h3>
     <div>
-      <label for="question">How are you feeling today:  </label>
+      <label>How are you feeling today:  </label>
+      <div class="mood_options">
+        <div v-for="option in moodOptions" :key="option.value" class="mood_option">
+          <label :for="option.value">{{ option.text }}</label>
+          <input type="radio" :id="option.value" v-model="mood" :value="option.value">
+          <div class="circle"></div>
+        </div>
+      </div>
+    </div>
+    <div>
+      <label for="question">From 1 to 5, how would you rate your day:  </label>
       <input type="text" v-model="question" id="question">
     </div>
   </div>
 </template>
 
 <style scoped>
+.mood_options {
+  display: flex;
+  gap: 20px;
+}
+.mood_option {
+  text-align: center;
+}
+.mood-option input[type="radio"] {
+  display: none;
+}
+.mood-option circle {
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  border: 2px solid #000;
+  margin-top: 5px;
+  cursor: pointer;
+}
+.mood-option input[type="radio"]:checked + circle {
+  background-color: #000;
+}
+
 h1 {
   font-weight: 500;
   font-size: 2.6rem;
